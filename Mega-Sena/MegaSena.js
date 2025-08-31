1e312b105e58b727b0e95813c57b33e08a122a07
RobertoDias <ra48273551857@acaonsfatima.org.br>
2025-08-31 18:56:22 -0300

    31/08

---
+++ Mega-Sena/MegaSena.js
@@ -0,0 +1,17 @@
+import prompt from"prompt-sync"
+let read = prompt()
+
+let random1 = (Math.random(1.60)*60)+1
+let random2 = (Math.random(1.60)*60)+1
+let random3 = (Math.random(1.60)*60)+1
+let random4 = (Math.random(1.60)*60)+1
+let random5 = (Math.random(1.60)*60)+1
+let random6 = (Math.random(1.60)*60)+1
+
+
+console.log(random1.toFixed(0))
+console.log(random2.toFixed(0))
+console.log(random3.toFixed(0))
+console.log(random4.toFixed(0))
+console.log(random5.toFixed(0))
+console.log(random6.toFixed(0))
